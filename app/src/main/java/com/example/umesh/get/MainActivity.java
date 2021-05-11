9b185a237f2864061c1ecbf84fad089473edebec
Umesh Manolkar <umeshvmanolkar@gmail.com>
2021-05-11 16:52:32 +0530

    testing
    testing

--- app/src/main/java/com/example/umesh/get/MainActivity.java
+++ app/src/main/java/com/example/umesh/get/MainActivity.java
@@ -20,7 +20,7 @@ import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+// import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
